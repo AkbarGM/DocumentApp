a213622f38f084ea8c255f65253ec1f7581db639
AkbarGM <56382572+AkbarGM@users.noreply.github.com>
2020-04-10 11:44:29 +0500

    Add project files.

---
+++ DocumentApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentApp
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            DocumentWorker dw;
+            Console.Write("Введите ключ: ");
+            string key = Console.ReadLine();
+
+            if (key=="pro")
+                dw = new ProDocumentWorker();
+            else if (key=="exp")
+                dw = new ExpertDocumentWorker();
+            else
+                dw = new DocumentWorker();
+
+            dw.OpenDocument();
+            dw.EditDocument();
+            dw.SaveDocument();
+            Console.ReadLine();
+        }
+    }
+}
